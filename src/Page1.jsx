890c9a07e9f4f9002d8a80a2d910d01ee7e5ae62
BennyKwan <bennyconten.tech@gmail.com>
2024-10-02 18:23:33 +0800

    test

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,93 @@
+import { rsvp } from "./rewards";
+import { useScrollWithShadow } from "./useScrollWithShadow";
+
 const Page1 = () => {
-    return (  
-        <div>asdasd</div>
+  const RewardItem = ({ title, buttonText, imgSrc }) => {
+    return (
+      <div
+      style={{
+        position: "relative",
+        zIndex: -1,
+      }}
+      >
+        <img
+          src="/webp/box.webp"
+          alt="ticket"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+        />
+        <img
+          src={imgSrc}
+          alt={title}
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
     );
-}
- 
+  };
+
+    const { boxShadow, onScrollHandler } = useScrollWithShadow();
+
+  return (
+    <div>
+      <div
+        style={{
+          height: "2.5svh",
+          alignContent: "left",
+          marginRight: "50%",
+        }}
+      >
+        <img
+          src="/webp/logo.webp"
+          alt="logo"
+          style={{
+            height: "100%",
+            width: "auto",
+          }}
+        />
+      </div>
+      <h1
+        style={{
+          fontSize: "2rem",
+          textAlign: "left",
+          lineHeight: "1.8rem",
+        }}
+      >
+        Welcome, <br />
+        Sarah
+      </h1>
+      <p
+        style={{
+          fontSize: "0.8rem",
+          textAlign: "left",
+          lineHeight: "1.2rem",
+        }}
+      >
+        Find your ticket below and redeem exclusive perks throughtout the space.
+        Enjoy!
+      </p>
+      <div
+      onScroll={onScrollHandler}
+        style={{
+            boxShadow,
+          overflowY: "auto",
+          height: "40svh",     
+          zIndex: 2,     
+        }}
+      >
+        {rsvp.map((reward, index) => (
+          <RewardItem
+            title={reward.title}
+            buttonText={reward.buttonText}
+            imgSrc={reward.imgSrc}
+            blurfilter={reward.disableBlur}
+            key={reward.title + index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Page1;
---
+++ src/rewards.jsx
@@ -0,0 +1,54 @@
+export const rsvp = [
+    {
+      title: "EXCLUSIVE-PASS",
+      buttonText: "Enter ",
+      imgSrc: "/webp/ticket.webp",
+      disableBlur: false,
+    },
+    {
+      title: "THE COACH COFFEE SHOP MINI SOFT SERVE",
+      buttonText: "Claim",
+      imgSrc: "/webp/cafe.webp",
+      disableBlur: true,
+    },
+    {
+      title: "PHOTOBOOTH",
+      buttonText: "Claim",
+      imgSrc: "/webp/photobooth.webp",
+      disableBlur: true,
+    },
+    {
+      title: "CLAWING MACHINE",
+      buttonText: "Claim",
+      imgSrc: "/webp/claw_machine.webp",
+      disableBlur: true,
+    },
+    {
+      title: "5% DISCOUNT",
+      buttonText: "Claim",
+      imgSrc: "/webp/discount.webp",
+      disableBlur: true,
+    },
+  ]
+  
+export const noneRsvp =  [
+    {
+      title: "GENERAL-PASS",
+      buttonText: "Enter",
+      imgSrc: "/webp/ticket.webp",
+      disableBlur: true,
+    },
+    {
+      title: "PHOTOBOOTH",
+      buttonText: "Claim",
+      imgSrc: "/webp/photobooth.webp",
+      disableBlur: true,
+    },
+    {
+      title: "5% DISCOUNT",
+      buttonText: "Claim",
+      imgSrc: "/webp/discount.webp",
+      disableBlur: true,
+    },
+
+];
